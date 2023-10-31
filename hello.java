e8ad213d07338af4918c9a1c0d54286d4cf29c66
Yash Kumar <62461690+YashGitExplorer@users.noreply.github.com>
2023-10-31 11:59:34 +0530

    Hello java file uploaded

---
+++ hello.java
@@ -0,0 +1,6 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java app \n by using Jenkins");  
+    System.out.println("Latest ");	 
+    }  
+    }  
