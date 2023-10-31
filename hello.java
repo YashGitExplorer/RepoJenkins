6c4ff0ca89081c1f1a0935a778aa06b493cf2569
Yash Kumar <62461690+YashGitExplorer@users.noreply.github.com>
2023-10-31 12:25:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class Hello{
     public static void main(String[] args){  
     System.out.println("This is java app \n by using Jenkins");  
     System.out.println("Latest ");	
-        System.out.println("Checking Changes worked");
+        System.out.println("Checking Changes");
     }  
     }  
